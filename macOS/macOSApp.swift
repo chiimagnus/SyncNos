df7023bad29aee9276207e48f6ae9d89660d12d7
𝓒𝓱𝓲𝓲 𝓜𝓪𝓰𝓷𝓾𝓼 <151696847+chiimagnus@users.noreply.github.com>
2025-09-10 13:03:31 +0800

    feat(macOS): 添加 macOS 应用目标
    - 在项目中添加了 macOS 应用目标，包括新的目标文件、资源文件和 entitlements 文件
    - 创建了基本的 SwiftUI 视图和应用入口文件
    - 调整了项目设置，支持 macOS 应用的开发和部署

---
+++ macOS/macOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  macOSApp.swift
+//  macOS
+//
+//  Created by chii_magnus on 2025/9/10.
+//
+
+import SwiftUI
+
+@main
+struct macOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
