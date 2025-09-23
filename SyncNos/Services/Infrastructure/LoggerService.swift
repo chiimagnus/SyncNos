e0e5848dbe33c45c59b7a622887f94bd38ead9a9
𝓒𝓱𝓲𝓲 𝓜𝓪𝓰𝓷𝓾𝓼 <151696847+chiimagnus@users.noreply.github.com>
2025-09-23 22:28:03 +0800

    feat(LoggerService): 添加日志服务实现
    - 新增 LoggerService 类及其协议 LoggerServiceProtocol，提供多种日志级别（verbose, debug, info, warning, error）的支持。
    - 实现日志记录功能，允许根据当前日志级别输出日志信息。
    - 在开发环境中默认设置为 debug 级别，生产环境中为 info 级别。
    - 提供日志输出到控制台的功能，并预留将日志写入文件的逻辑。

---
+++ SyncNos/Services/Infrastructure/LoggerService.swift
@@ -0,0 +1,88 @@
+import Foundation
+
+// MARK: - Logger Level
+enum LogLevel: Int, CaseIterable, Comparable {
+    case verbose = 0
+    case debug = 1
+    case info = 2
+    case warning = 3
+    case error = 4
+
+    static func < (lhs: LogLevel, rhs: LogLevel) -> Bool {
+        return lhs.rawValue < rhs.rawValue
+    }
+
+    var description: String {
+        switch self {
+        case .verbose: return "VERBOSE"
+        case .debug: return "DEBUG"
+        case .info: return "INFO"
+        case .warning: return "WARNING"
+        case .error: return "ERROR"
+        }
+    }
+}
+
+// MARK: - Logger Service Protocol
+protocol LoggerServiceProtocol {
+    var currentLevel: LogLevel { get set }
+
+    func log(_ level: LogLevel, message: String, file: String, function: String, line: Int)
+
+    func verbose(_ message: String, file: String, function: String, line: Int)
+    func debug(_ message: String, file: String, function: String, line: Int)
+    func info(_ message: String, file: String, function: String, line: Int)
+    func warning(_ message: String, file: String, function: String, line: Int)
+    func error(_ message: String, file: String, function: String, line: Int)
+}
+
+// MARK: - Logger Service Implementation
+class LoggerService: LoggerServiceProtocol {
+    static let shared = LoggerService()
+
+    private init() {
+        // 在开发环境中默认为debug级别，生产环境中默认为info级别
+        #if DEBUG
+        self.currentLevel = .debug
+        #else
+        self.currentLevel = .info
+        #endif
+    }
+
+    var currentLevel: LogLevel = .info
+
+    func log(_ level: LogLevel, message: String, file: String = #file, function: String = #function, line: Int = #line) {
+        // 只有当日志级别大于等于当前配置级别时才输出
+        guard level >= currentLevel else { return }
+
+        let fileName = (file as NSString).lastPathComponent
+        let timestamp = DateFormatter.localizedString(from: Date(), dateStyle: .none, timeStyle: .medium)
+        let logMessage = "\(timestamp) [\(level.description)] \(fileName):\(line) \(function) - \(message)"
+
+        // 输出到控制台
+        print(logMessage)
+
+        // TODO: 在生产环境中可以添加文件记录功能
+        // 这里可以添加将日志写入文件的逻辑
+    }
+
+    func verbose(_ message: String, file: String = #file, function: String = #function, line: Int = #line) {
+        log(.verbose, message: message, file: file, function: function, line: line)
+    }
+
+    func debug(_ message: String, file: String = #file, function: String = #function, line: Int = #line) {
+        log(.debug, message: message, file: file, function: function, line: line)
+    }
+
+    func info(_ message: String, file: String = #file, function: String = #function, line: Int = #line) {
+        log(.info, message: message, file: file, function: function, line: line)
+    }
+
+    func warning(_ message: String, file: String = #file, function: String = #function, line: Int = #line) {
+        log(.warning, message: message, file: file, function: function, line: line)
+    }
+
+    func error(_ message: String, file: String = #file, function: String = #function, line: Int = #line) {
+        log(.error, message: message, file: file, function: function, line: line)
+    }
+}
