eb54187f5421557c371ac78aa1794278f6009757
𝓒𝓱𝓲𝓲 𝓜𝓪𝓰𝓷𝓾𝓼 <151696847+chiimagnus@users.noreply.github.com>
2025-10-03 12:14:26 +0800

    fix: 注释掉 GoodLinksDetailView 中的 frame 设置

--- SyncNos/Views/GoodLinks/GoodLinksDetailView.swift
+++ SyncNos/Views/GoodLinks/GoodLinksDetailView.swift
@@ -170,7 +170,7 @@ struct GoodLinksDetailView: View {
                                             .stroke(Color.secondary.opacity(0.08), lineWidth: 1)
                                     )
                             }
-                            .frame(maxWidth: frozenLayoutWidth, alignment: .leading)
+                            // .frame(maxWidth: frozenLayoutWidth, alignment: .leading)
                         }
 
                         // 高亮列表
