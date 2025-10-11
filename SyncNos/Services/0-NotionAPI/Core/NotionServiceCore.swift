12e8978303187581964b06df01c6013b3b391f36
𝓒𝓱𝓲𝓲 𝓜𝓪𝓰𝓷𝓾𝓼 <151696847+chiimagnus@users.noreply.github.com>
2025-10-11 23:02:31 +0800

    Revert "feat(notion-api): - 升级 Notion API 版本至 2025-09-03"
    This reverts commit 08c1caf6ff7b773f263d0b2f80c9e0d1ce15ea45.

--- SyncNos/Services/0-NotionAPI/Core/NotionServiceCore.swift
+++ SyncNos/Services/0-NotionAPI/Core/NotionServiceCore.swift
@@ -5,7 +5,7 @@ class NotionServiceCore {
     let configStore: NotionConfigStoreProtocol
     let logger = DIContainer.shared.loggerService
     let apiBase = URL(string: "https://api.notion.com/v1/")!
-    let notionVersion = "2025-09-03"
+    let notionVersion = "2022-06-28"
 
     // ISO8601 formatter for highlight timestamps when syncing to Notion
     static let isoDateFormatter: ISO8601DateFormatter = {
