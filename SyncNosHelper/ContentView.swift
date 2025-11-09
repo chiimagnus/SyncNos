d012905d33cd27ebf8cffb6b156b5dc04b86df56
𝓒𝓱𝓲𝓲 𝓜𝓪𝓰𝓷𝓾𝓼 <151696847+chiimagnus@users.noreply.github.com>
2025-11-09 20:27:33 +0800

    feat: add SyncNosHelper app and initial setup
    - Introduced a new target for SyncNosHelper, including the main application file and content view.
    - Added asset catalog for app icons and accent colors to enhance the visual identity of the helper app.
    - Updated project configuration to include SyncNosHelper in the build settings and schemes for better integration.
    - This addition expands the functionality of SyncNos, providing users with a dedicated helper application.

---
+++ SyncNosHelper/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SyncNosHelper
+//
+//  Created by chii_magnus on 2025/11/9.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SyncNosHelper/SyncNosHelperApp.swift
@@ -0,0 +1,17 @@
+//
+//  SyncNosHelperApp.swift
+//  SyncNosHelper
+//
+//  Created by chii_magnus on 2025/11/9.
+//
+
+import SwiftUI
+
+@main
+struct SyncNosHelperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
