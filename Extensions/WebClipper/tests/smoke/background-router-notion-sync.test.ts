9910a7135386cf7dc6abeef86ff15c1d08c1403b
𝓒𝓱𝓲𝓲 𝓜𝓪𝓰𝓷𝓾𝓼 <151696847+chiimagnus@users.noreply.github.com>
2026-02-26 15:26:27 +0800

    webclipper: move protocol modules to src/protocols

--- Extensions/WebClipper/tests/smoke/background-router-notion-sync.test.ts
+++ Extensions/WebClipper/tests/smoke/background-router-notion-sync.test.ts
@@ -33,18 +33,18 @@ function mockChromeStorage({ parentPageId = "parent_page" } = {}) {
 function loadBackgroundRouter() {
   // Ensure global protocol modules are loaded before orchestrator (mirrors MV3 load order).
   // eslint-disable-next-line @typescript-eslint/no-var-requires
-  const kindContractPath = require.resolve("../../src/shared/conversation-kind-contract.js");
+  const kindContractPath = require.resolve("../../src/protocols/conversation-kind-contract.js");
   // eslint-disable-next-line @typescript-eslint/no-dynamic-delete
   delete require.cache[kindContractPath];
   // eslint-disable-next-line @typescript-eslint/no-var-requires
-  require("../../src/shared/conversation-kind-contract.js");
+  require("../../src/protocols/conversation-kind-contract.js");
 
   // eslint-disable-next-line @typescript-eslint/no-var-requires
-  const kindsPath = require.resolve("../../src/shared/conversation-kinds.js");
+  const kindsPath = require.resolve("../../src/protocols/conversation-kinds.js");
   // eslint-disable-next-line @typescript-eslint/no-dynamic-delete
   delete require.cache[kindsPath];
   // eslint-disable-next-line @typescript-eslint/no-var-requires
-  require("../../src/shared/conversation-kinds.js");
+  require("../../src/protocols/conversation-kinds.js");
 
   // eslint-disable-next-line @typescript-eslint/no-var-requires
   const jobStoreModulePath = require.resolve("../../src/sync/notion/notion-sync-job-store.js");
