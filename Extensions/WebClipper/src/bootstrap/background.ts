1f02300ce7c8e6685e22c083edcaabcc87b55717
𝓒𝓱𝓲𝓲 𝓜𝓪𝓰𝓷𝓾𝓼 <151696847+chiimagnus@users.noreply.github.com>
2026-03-05 02:28:14 +0800

    chore(inpage): remove unused background visibility plumbing

--- Extensions/WebClipper/src/bootstrap/background.ts
+++ Extensions/WebClipper/src/bootstrap/background.ts
@@ -2,11 +2,5 @@ import { createBackgroundServices } from './background-services.ts';
 
 export function startBackgroundBootstrap() {
   const services = createBackgroundServices();
-  try {
-    services.backgroundInpageWebVisibility?.start?.();
-  } catch (_e) {
-    // ignore
-  }
-
   return services;
 }
