b81406f7e76f68ef480c4dbb45f463b1e2b90ee7
𝓒𝓱𝓲𝓲 𝓜𝓪𝓰𝓷𝓾𝓼 <151696847+chiimagnus@users.noreply.github.com>
2026-03-26 19:34:53 +0800

    style(tooltip): 更新工具提示样式，优化视觉表现

--- webclipper/src/entrypoints/popup/main.tsx
+++ webclipper/src/entrypoints/popup/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import PopupShell from '@ui/popup/PopupShell';
 import '@ui/styles/tokens.css';
 import '@ui/styles/buttons.css';
+import 'react-tooltip/dist/react-tooltip.css';
 import '@ui/styles/tooltip.css';
 import '@ui/styles/tailwind.css';
 import '@entrypoints/popup/style.css';
--- webclipper/src/ui/shared/AppTooltip.tsx
+++ webclipper/src/ui/shared/AppTooltip.tsx
@@ -1,5 +1,4 @@
 import { Tooltip } from 'react-tooltip';
-import 'react-tooltip/dist/react-tooltip.css';
 
 const WEBCLIPPER_TOOLTIP_ID = 'webclipper-tooltip';
 
