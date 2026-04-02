9c44e37634921f20fa20076898e7a6ff56b0325c
𝓒𝓱𝓲𝓲 𝓜𝓪𝓰𝓷𝓾𝓼 <151696847+chiimagnus@users.noreply.github.com>
2026-04-02 22:35:25 +0800

    refactor(webclipper): remove xhs web article adapter

--- webclipper/src/collectors/web/article-fetch-sites/index.ts
+++ webclipper/src/collectors/web/article-fetch-sites/index.ts
@@ -1,6 +1,4 @@
 import type { ArticleFetchSiteSpec } from '@collectors/web/article-fetch-sites/site-spec';
 import { BILIBILI_OPUS_SITE_SPEC } from '@collectors/web/article-fetch-sites/bilibili-opus';
-import { XHS_NOTE_SITE_SPEC } from '@collectors/web/article-fetch-sites/xhs-note';
 
-export const ARTICLE_FETCH_SITE_SPECS: ArticleFetchSiteSpec[] = [BILIBILI_OPUS_SITE_SPEC, XHS_NOTE_SITE_SPEC];
-
+export const ARTICLE_FETCH_SITE_SPECS: ArticleFetchSiteSpec[] = [BILIBILI_OPUS_SITE_SPEC];
