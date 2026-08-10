71798bdec3666746a542543e3ecabf44dc4f18be
𝓒𝓱𝓲𝓲 𝓜𝓪𝓰𝓷𝓾𝓼 <151696847+chiimagnus@users.noreply.github.com>
2026-08-10 17:14:48 +0800

    1.9.4

--- wxt.config.ts
+++ wxt.config.ts
@@ -60,7 +60,7 @@ const resolveManifest: UserManifestFn = (env) => {
 
   return {
     name: isSafari ? '__MSG_name__' : '__MSG_extName__',
-    version: '1.9.3',
+    version: '1.9.4',
     description: isSafari ? '__MSG_description__' : '__MSG_extDescription__',
     default_locale: 'en',
     permissions,
